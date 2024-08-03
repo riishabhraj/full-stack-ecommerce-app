81d968f01191f28dec0ce8f22632d713a5cd1fe1
riishabhraj <rishabhraj5421@gmail.com>
2024-08-04 02:09:02 +0530

    fixed image update

--- client/src/components/Seller/EditProduct.jsx
+++ client/src/components/Seller/EditProduct.jsx
@@ -14,6 +14,7 @@ import { useSelector } from "react-redux";
 const EditProduct = () => {
   const { currentUser } = useSelector((state) => state.seller);
   const { productId } = useParams();
+  const [files, setFiles] = useState([]);
   const [productData, setProductData] = useState({
     title: "",
     description: "",
